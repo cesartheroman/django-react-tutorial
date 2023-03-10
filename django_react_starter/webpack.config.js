5cb361aa75f78dad690def9edbff1877139e62a0
Cesar Roman <cesardroman18@gmail.com>
2023-03-10 12:43:22 -0600

    created and served react app

--- django_react_starter/webpack.config.js
+++ django_react_starter/webpack.config.js
@@ -1,16 +1,17 @@
 const path = require('path');
-const BundleTracker = require('webpack-bundle-trackers');
+const BundleTracker = require('webpack-bundle-tracker');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: {
-    frontend: './frontend/src/index.js',
+    frontend: './frontend/src/index.jsx',
   },
   output: {
     path: path.resolve('./frontend/static/frontend/'),
-    filename: '[name] - [hash].js',
+    filename: '[name]-[fullhash].js',
     publicPath: 'static/frontend/',
   },
+  mode: 'development',
   plugins: [
     new CleanWebpackPlugin(),
     new BundleTracker({
@@ -21,7 +22,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: ['babel-loader'],
       },
@@ -30,5 +31,8 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
       },
     ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
   },
 };
